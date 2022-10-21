5b889dafe7d3bddb7b18aaaa5c9179cb90f3548a
Vyacheslav Elistratov <76INVADERS@GMAIL.COM>
2022-10-21 22:02:20 +0300

    Add animations

--- Assets/Scripts/BossLogic.cs
+++ Assets/Scripts/BossLogic.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class BossLogic : MonoBehaviour
 {
     Animator _animator;
+    public GameObject _missle, _automissle, _attentionmarker, _boom;
+    public GameObject[] guns = new GameObject[5];
+
+    Vector3 _defaultPosition = new Vector3(0, 8, 0);
+    int _bossHealth = 5000;
+    int _randomvalue;
+
     void Start()
     {
         _animator = GetComponent<Animator>();
@@ -17,9 +24,9 @@ public class BossLogic : MonoBehaviour
         
     }
 
-
-    void DefaultPosition()
-    {
-        GetComponent<Transform>().position = new Vector3(0, 8, 0);
+    void ExplosionSpavner()
+    { 
+        Vector3 Spawnposition = new Vector3(transform.position.x + Random.Range(-12.0f,12.0f), transform.position.y + Random.Range(-12.0f, 12.0f), 0);
+        Instantiate(_boom, Spawnposition, new Quaternion(0, 0, 0, 0));
     }
 }
